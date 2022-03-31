65064ce2b4698d8abcd42feda29b966f643a4b4b
sudha-d <89769108+sudha-d@users.noreply.github.com>
2022-03-31 10:25:50 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -5,7 +5,7 @@ sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum install epel-release 
 amazon-linux-extras install epel
 amazon-linux-extras install java-openjdk11
-sudo install java-11-openjdk-devel
+sudo yum install java-11-openjdk-devel
 sudo yum install jenkins
 systemctl  start jenkins
 cat /var/lib/jenkins/secrets/initialAdminPassword
